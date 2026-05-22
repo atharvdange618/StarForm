2e551ab52d53eecc3242372c23d04800c87e71e5
Atharv Dange <atharvdange618@gmail.com>
2026-05-22 18:24:35 +0530

    style(web): refactor skeleton, tooltip, and progress components to apply soft corners

--- apps/web/components/ui/progress.tsx
+++ apps/web/components/ui/progress.tsx
@@ -1,28 +1,24 @@
 'use client';
 
-import * as React from 'react';
 import { Progress as ProgressPrimitive } from 'radix-ui';
 
 import { cn } from '@/lib/utils';
 
 function Progress({
-  className,
   value,
+  className,
   ...props
 }: React.ComponentProps<typeof ProgressPrimitive.Root>) {
   return (
     <ProgressPrimitive.Root
       data-slot="progress"
-      className={cn(
-        'relative flex h-1 w-full items-center overflow-x-hidden rounded-none bg-muted',
-        className,
-      )}
+      className={cn('relative h-2 w-full overflow-hidden rounded-full bg-primary/20', className)}
       {...props}
     >
       <ProgressPrimitive.Indicator
         data-slot="progress-indicator"
         className="size-full flex-1 bg-primary transition-all"
-        style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+        style={{ transform: `translateX(-${100 - (value ?? 0)}%)` }}
       />
     </ProgressPrimitive.Root>
   );
